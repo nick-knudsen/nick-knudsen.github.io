d1aa5ef4ebb41ec09352dd459e60659b7a243e77
Nick Knudsen <nick.knudsen14@gmail.com>
2024-07-11 13:15:37 -0400

    restoring accidentally overwritten file

--- public/js/cocktails.js
+++ public/js/cocktails.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     let scrollElementIndex = 0;
+    const images = document.querySelectorAll('.image-gallery .image-item');
+    
+    // this doesn't work because of timing for some reason
+
+    // if (window.innerWidth >= 900) {
+    //     let totalHeight = 0;
+    //     for (let i = 0; i < images.length; i++) {
+    //         totalHeight += images[i].offsetHeight;
+    //     }
+
+    //     document.getElementById('image-gallery').style.height = totalHeight/3 + 100 + 'px';
+    //     console.log(totalHeight/3 + 100 + 'px');
+    // }
 
     function getVisibleElementIndex() {
-        const images = document.querySelectorAll('.image-item');
         for (let i = 0; i < images.length; i++) {
             const rect = images[i].getBoundingClientRect();
             if (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
@@ -13,7 +25,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function scrollToElement(index) {
-        const images = document.querySelectorAll('.image-item');
         if (images[index]) {
             images[index].scrollIntoView();
         }
@@ -21,8 +32,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function selectTileView() {
         scrollElementIndex = getVisibleElementIndex();
-        document.querySelector('.gallery-container').classList.remove('timeline-view');
-        document.querySelector('.gallery-container').classList.add('tile-view');
+        document.querySelector('.image-gallery').classList.remove('timeline-view');
+        document.querySelector('.image-gallery').classList.add('tile-view');
         document.getElementById('tile-view-button').classList.add('active');
         document.getElementById('timeline-view-button').classList.remove('active');
         scrollToElement(scrollElementIndex);
@@ -30,8 +41,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function selectTimelineView() {
         scrollElementIndex = getVisibleElementIndex();
-        document.querySelector('.gallery-container').classList.remove('tile-view');
-        document.querySelector('.gallery-container').classList.add('timeline-view');
+        document.querySelector('.image-gallery').classList.remove('tile-view');
+        document.querySelector('.image-gallery').classList.add('timeline-view');
         document.getElementById('timeline-view-button').classList.add('active');
         document.getElementById('tile-view-button').classList.remove('active');
         scrollToElement(scrollElementIndex);
